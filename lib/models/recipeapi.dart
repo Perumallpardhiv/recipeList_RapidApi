984531e39d2aafb8c1925b5673d7668ea15792f5
Perumallpardhiv <LIT2021001@iiitl.ac.in>
2023-02-07 00:47:32 +0530

    Hiding API Key

--- lib/models/recipeapi.dart
+++ lib/models/recipeapi.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:rapid_api/secrete_apikey.dart';
 import 'package:http/http.dart' as http;
 import 'package:rapid_api/models/recipe.dart';
 
@@ -9,7 +9,7 @@ class RecipeApiClass {
 
     // var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list', {'limit': '24', 'start': '0'});
     final response = await http.get(uri, headers: {
-      "X-RapidAPI-Key": "d6c16df18bmsha63dbb82162dc4dp1bff5ejsn48f5a5a27684",
+      "X-RapidAPI-Key": apiKey,
       "X-RapidAPI-Host": "yummly2.p.rapidapi.com",
       "useQueryString": "true"
     });
